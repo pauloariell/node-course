544bd1667c1342d85d9e9dfaa7ae01a110ea3cce
PAC <PAC@NBPAC>
2020-05-06 00:12:52 -0300

    Reiniciando projeto node api

--- Server.js
+++ Server.js
@@ -1,21 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
-//Iniciando o APP
 const app = express();
 
-//Iniciando o DB
-mongoose.connect(
-    'mongodb://localhost:27017/nodeapi',
-    { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-);
-
-//Primeira Ro
 app.get('/', (req, res) => {
-    res.send("Hello uordi!");
+  res.send('Ohayou PACcode!');
 });
 
 app.listen(3001);
